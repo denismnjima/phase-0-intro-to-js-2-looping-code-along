302597ee32a449cd4c3839e406688a7d47a3fbdb
denismnjima <denismnjima@gmail.com>
2024-07-11 13:53:28 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
-function writeCards(names, occasion) {
-    return names.map((name) => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+function writeCards(names, gift) {
+    return names.map((name) => `Thank you, ${name}, for the wonderful ${gift} gift!`);
   }
   function countDown(startingNumber) {
     for (let i = startingNumber; i >= 0; i--) {
